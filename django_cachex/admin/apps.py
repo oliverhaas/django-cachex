dbd4243d730f6cb5c82b544421c48138cab72b12
Oliver Haas <ohaas@e1plus.de>
2026-02-07 13:42:55 +0100

    cleanup: remove unnecessary default_auto_field from AppConfig classes
    These apps have no models, so default_auto_field has no effect.

--- django_cachex/admin/apps.py
+++ django_cachex/admin/apps.py
@@ -4,7 +4,6 @@ from django.apps import AppConfig
 class CacheAdminConfig(AppConfig):
     """Django app configuration for the cache admin interface."""
 
-    default_auto_field = "django.db.models.BigAutoField"
     name = "django_cachex.admin"
     label = "django_cachex"
     verbose_name = "django-cachex"
--- django_cachex/unfold/apps.py
+++ django_cachex/unfold/apps.py
@@ -4,7 +4,6 @@ from django.apps import AppConfig
 class UnfoldCacheAdminConfig(AppConfig):
     """Django app configuration for the unfold-styled cache admin interface."""
 
-    default_auto_field = "django.db.models.BigAutoField"
     name = "django_cachex.unfold"
     label = "django_cachex"
     verbose_name = "django-cachex"
